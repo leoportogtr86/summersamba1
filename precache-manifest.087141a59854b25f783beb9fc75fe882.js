36cf4b3995257d25c2267fce6ab09a9a5a0b5b6c
Leo Porto <leonardoreisporto@gmail.com>
2020-06-20 05:38:46 -0300

    Updates

---
+++ precache-manifest.087141a59854b25f783beb9fc75fe882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77faadad2405c72b226c540086e86550",
+    "url": "/summersamba1/index.html"
+  },
+  {
+    "revision": "cc6803ff986753743ac4",
+    "url": "/summersamba1/static/css/main.f7a160da.chunk.css"
+  },
+  {
+    "revision": "09410e69275b35fe76a2",
+    "url": "/summersamba1/static/js/2.6617b313.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/summersamba1/static/js/2.6617b313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc6803ff986753743ac4",
+    "url": "/summersamba1/static/js/main.a6845229.chunk.js"
+  },
+  {
+    "revision": "efb98ef623426a0fdecc",
+    "url": "/summersamba1/static/js/runtime-main.9364c733.js"
+  }
+]);
