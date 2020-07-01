134ed3b3b3db61383f167673699719e5b8a32c3d
Leo Porto <leonardoreisporto@gmail.com>
2020-07-01 13:22:31 -0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,8 +10,8 @@ export default props=>{
 
         <div>
 
-            <h1 className = 'text-center text-danger'>Summer Samba - Line 1</h1>
-            <Tab src = 'https://www.soundslice.com/slices/DmpVc/embed-channelpost'/>
+            <h1 className = 'text-center text-danger'>Summer Samba - Aproximação Cromática - b3 3 5 7</h1>
+            <Tab src = 'https://www.soundslice.com/slices/LRcfc/embed-channelpost/'/>
             <Backing src = 'https://www.youtube.com/embed/3h92WSJc-pw'/>
             
             
